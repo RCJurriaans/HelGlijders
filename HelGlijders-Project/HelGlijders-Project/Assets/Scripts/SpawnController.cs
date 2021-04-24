953514ce98f4986f0f648464c1acfdf3e7590610
deKroon <max.de.kroon93@gmail.com>
2021-04-24 16:37:39 +0200

    added camera colors, lighting and fog

--- HelGlijders-Project/HelGlijders-Project/Assets/Scripts/SpawnController.cs
+++ HelGlijders-Project/HelGlijders-Project/Assets/Scripts/SpawnController.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class SpawnController : MonoBehaviour
 {
     public GameObject FloorPrefab;
+    public GameObject ObstaclePrefab;
+    [SerializeField] private Transform Geometry;
     [SerializeField] private Transform SpawnPosition;
 
     void Start()
     {
         transform.localRotation = Quaternion.AngleAxis(45 + Random.value*-10f, Vector3.right);
+
+        for (int i = 0; i < 2; i++)
+        {
+            Vector3 randomPosition = RandomPointInBox(Geometry.GetComponent<BoxCollider>().bounds);
+            Instantiate(ObstaclePrefab, randomPosition, transform.rotation);
+        }
     }
 
     public void SpawnNextFloor()
@@ -21,4 +29,15 @@ public class SpawnController : MonoBehaviour
     {
         Destroy(gameObject);
     }
+
+    private Vector3 RandomPointInBox(Bounds bounds)
+    {
+        return new Vector3(
+            Random.Range(bounds.min.x, bounds.max.x),
+            Random.Range(bounds.min.y, bounds.max.y),
+            Random.Range(bounds.min.z, bounds.max.z)
+        );
+    }
+
+
 }
