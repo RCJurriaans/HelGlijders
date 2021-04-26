da1dd6fc8fcf84b6e21a52d4e0b4c2f9ecec3d68
deKroon <max.de.kroon93@gmail.com>
2021-04-26 13:42:47 +0200

    added rock obstacles and floor texture

--- HelGlijders-Project/HelGlijders-Project/Assets/Scripts/SpawnController.cs
+++ HelGlijders-Project/HelGlijders-Project/Assets/Scripts/SpawnController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class SpawnController : MonoBehaviour
 {
     public GameObject FloorPrefab;
-    public GameObject ObstaclePrefab;
+    public GameObject[] ObstaclePrefabs;
     [SerializeField] private Transform Geometry;
     [SerializeField] private Transform SpawnPosition;
+
+    int randomInt;
 
     private List<GameObject> obstacles;
 
@@ -18,10 +20,9 @@ public class SpawnController : MonoBehaviour
 
         obstacles = new List<GameObject>();
 
-        for(int i=0; i<3; i++)
+        for(int i=0; i<6; i++)
         {
-            GameObject newObstacle = Instantiate(ObstaclePrefab, getRandomPosition(), Geometry.rotation);
-            obstacles.Add(newObstacle);
+            obstacles.Add(SpawnRandom());
         }
 
     }   
@@ -41,7 +42,15 @@ public class SpawnController : MonoBehaviour
     }
     Vector3 getRandomPosition()
     {
-        Vector3 randomPoint = Geometry.TransformPoint(new Vector3(Random.Range(-0.5f, 0.5f), 1f, Random.Range(-0.5f, 0.5f)));
+        Vector3 randomPoint = Geometry.TransformPoint(new Vector3(Random.Range(-0.5f, 0.5f), 0.5f, Random.Range(-0.5f, 0.5f)));
         return randomPoint;
+    }
+
+    GameObject SpawnRandom()
+    {
+        randomInt = Random.Range(0, ObstaclePrefabs.Length);
+        GameObject randomObstacle = Instantiate(ObstaclePrefabs[randomInt], getRandomPosition(), Geometry.rotation * new Quaternion(0f, Random.value * 1f, 0f, 1f));
+        randomObstacle.transform.localScale = new Vector3(1.25f -Random.value*.5f, 1.25f-Random.value*.5f, 1.25f-Random.value*.5f);
+        return randomObstacle;
     }
 }
